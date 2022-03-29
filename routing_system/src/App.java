adc9aa8a88f324f35c15a82f58a6c2f24826672f
Bobby-gh <knaihv@ymail.com>
2022-03-29 22:01:15 +0000

    VogelApproximation

---
+++ routing_system/src/App.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import routing_system.GraphA;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Connecting vertices...");
+
+        ArrayList<Object> vertices = new ArrayList<>();
+        vertices.add("Great Hall");
+        vertices.add("School of Graduate Studies");
+        vertices.add("Volta Hall");
+        vertices.add("Balme Library");
+        vertices.add("New N Block");
+        vertices.add("Department of Political Science");
+        vertices.add("School of Engineering");
+        vertices.add("Department of Mathematics");
+        vertices.add("UGBS");
+        vertices.add("UGCS");
+        vertices.add("Chemistry Department");
+        vertices.add("International Programs Office");
+        vertices.add("JQB");
+        vertices.add("Department of Geography");
+        vertices.add("Legon Hall");
+        vertices.add("Akuafo Hall");
+        vertices.add("College of Basic and Applied Sciences");
+        vertices.add("Mensah Sarbah Hall");
+        vertices.add("Athletic Oval");
+        vertices.add("GCB");
+
+        GraphHashMap graphHM = new GraphHashMap(vertices);
+        graphHM.addEdge("Great Hall", "School of Graduate Studies", 500);
+        graphHM.addEdge("Great Hall", "Volta Hall", 500);
+        graphHM.addEdge("School of Graduate Studies", "Volta Hall", 300);
+        graphHM.addEdge("School of Graduate Studies", "UGBS", 290);
+        graphHM.addEdge("Volta Hall", "UGBS", 190);
+        graphHM.addEdge("Volta Hall", "Legon Hall", 190);
+        graphHM.addEdge("UGBS", "UGCS", 210);
+        graphHM.addEdge("UGBS", "GCB", 450);
+        graphHM.addEdge("GCB", "New N Block", 110);
+        graphHM.addEdge("New N Block", "Department of Political Science", 400);
+        graphHM.addEdge("Department of Political Science", "Department of Mathematics", 280);
+        graphHM.addEdge("Department of Mathematics", "Chemistry Department", 180);
+        graphHM.addEdge("UGBS", "Chemistry Department", 350);
+        graphHM.addEdge("UGCS", "Legon Hall", 240);
+        graphHM.addEdge("Legon Hall", "Mensah Sarbah Hall", 1200);
+        graphHM.addEdge("Legon Hall", "Athletic Oval", 800);
+        graphHM.addEdge("Legon Hall", "Akuafo Hall", 700);
+        graphHM.addEdge("Legon Hall", "Balme Library", 500);
+        graphHM.addEdge("Balme Library", "Akuafo Hall", 270);
+        graphHM.addEdge("Balme Library", "Chemistry Department", 400);
+        graphHM.addEdge("Mensah Sarbah Hall", "Athletic Oval", 400);
+        graphHM.addEdge("Mensah Sarbah Hall", "Akuafo Hall", 850);
+        graphHM.addEdge("Athletic Oval", "Akuafo Hall", 550);
+        graphHM.addEdge("College of Basic and Applied Sciences", "Akuafo Hall", 240);
+        graphHM.addEdge("College of Basic and Applied Sciences", "Department of Geography", 280);
+        graphHM.addEdge("JQB", "Department of Geography", 400);
+        graphHM.addEdge("JQB", "International Programs Office", 270);
+        graphHM.addEdge("School of Engineering", "International Programs Office", 350);
+        graphHM.addEdge("International Programs Office", "Chemistry Department", 400);
+        graphHM.addEdge("Chemistry Department", "Akuafo Hall", 450);
+
+        Gui UI = new Gui(vertices);
+
+        // graphHM.printGraph();
+        System.out.println("--------------------------------Depth First Traversal--------------------------------");
+        System.out.println();
+        graphHM.printAllPaths("Department of Mathematics", "Balme Library");
+
+        System.out.println("5 possible routes from " + "Source" + " to " + "Destination");
+        for (int i = 0; i < 5; i++) {
+            List<String> s = graphHM.allPaths.get(i);
+            System.out.println(String.join(" -> ", s));
+        }
+        System.out.println();
+
+        graphHM.listToMatrix();
+
+        GraphAdjacencyMatrix graphAM = new GraphAdjacencyMatrix(vertices.size());
+        graphAM.matrix = graphHM.matrix;
+
+        // graphAM.printGraph();
+
+        VogelApproximationMethod VAM = new VogelApproximationMethod(vertices.size(), graphAM.matrix);
+        for (int i = 0; i < vertices.size(); i++) {
+            VAM.calculatePenalties();
+            VAM.getProvisionalRoute();
+        }
+
+        // System.out.println("List of provisional routes: "+ VAM.provisionalRoute);
+        VAM.getSolutionCost();
+        graphHM.getShortestPath();
+
+    }
+
+}
---
+++ routing_system/src/GraphHashMap.java
@@ -0,0 +1,170 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+public class GraphHashMap {
+    HashMap<Object, LinkedList<Edge>> adjacencyList = new HashMap<>();
+    HashMap<Object, Integer> indexes = new HashMap<>();
+    int index = -1;
+    int listSize = 0;
+    List<List<String>> allPaths = new ArrayList<>();
+    int[][] matrix;
+
+    public GraphHashMap(ArrayList<Object> vertices) {
+        this.listSize = vertices.size();
+        this.matrix = new int[vertices.size()][vertices.size()];
+        for (int i = 0; i < vertices.size(); i++) {
+            Object vertex = vertices.get(i);
+            LinkedList<Edge> list = new LinkedList<>();
+            adjacencyList.put(vertex, list);
+            indexes.put(vertex, ++index);
+        }
+    }
+
+    public void addEdge(Object source, Object destination, Integer weight) {
+        // add forward edge
+        Edge edge = new Edge(source, destination, weight);
+        LinkedList<Edge> list;
+        list = adjacencyList.get(source);
+        list.addFirst(edge);
+        adjacencyList.put(source, list);
+
+        // add back edge
+        edge = new Edge(destination, source, weight);
+        list = adjacencyList.get(destination);
+        list.addFirst(edge);
+        adjacencyList.put(destination, list);
+
+    }
+
+    public void DFS(Object source) {
+        boolean[] visited = new boolean[adjacencyList.size()];
+
+        Stack<Object> stack = new Stack<>();
+        stack.push(source);
+        Object a = null;
+
+        while (!stack.empty()) {
+            source = stack.peek();
+            stack.pop();
+
+            if (visited[indexes.get(source)] == false) {
+                System.out.print(source + " ");
+                visited[indexes.get(source)] = true;
+            }
+
+            for (int i = 0; i < adjacencyList.get(source).size(); i++) {
+                a = adjacencyList.get(source).get(i).destination;
+                if (!visited[indexes.get(a)]) {
+                    stack.push(a);
+                }
+            }
+        }
+    }
+
+    public void printAllPaths(Object source, Object destination) {
+        allPaths = new ArrayList<>();
+        boolean[] visited = new boolean[adjacencyList.size()];
+        visited[indexes.get(source)] = true;
+        print(source, destination, "", visited);
+    }
+
+    public void print(Object source, Object destination, String path, boolean[] visited) {
+        String newPath = path + "->" + source;
+        visited[indexes.get(source)] = true;
+
+        LinkedList<Edge> list = adjacencyList.get(source);
+        for (int i = 0; i < list.size(); i++) {
+            Edge edge = list.get(i);
+            if (indexes.get(edge.destination) < adjacencyList.size() && edge.destination != destination
+                    && visited[indexes.get(edge.destination)] == false) {
+                print(edge.destination, destination, newPath, visited);
+            } else if (edge.destination == destination) {
+                // System.out.println(newPath+"->"+edge.destination);
+                String s = newPath + "->" + edge.destination;
+                List<String> temp = Arrays.asList(s.split("->"));
+                allPaths.add(temp);
+            }
+        }
+        visited[indexes.get(source)] = false;
+    }
+
+    public void listToMatrix() {
+        for (List<String> o : allPaths) {
+            for (String p : o) {
+                if (p != "" && o.indexOf(p) + 1 < o.size()) {
+                    LinkedList<Edge> LL = adjacencyList.get(p);
+                    Edge next = null;
+                    for (Edge e : LL) {
+                        if (e.destination.equals((o.get(o.indexOf(p) + 1)))) {
+                            next = e;
+                        }
+                    }
+                    matrix[indexes.get(p)][indexes.get(o.get(o.indexOf(p) + 1))] = next.weight;
+                }
+            }
+        }
+    }
+
+    public void printGraph() {
+        Set<Object> set = adjacencyList.keySet();
+        Iterator<Object> iterator = set.iterator();
+
+        System.out.println("Graph: (Adjacency List using Hash Maps)");
+        while (iterator.hasNext()) {
+            Object vertex = iterator.next();
+            System.out.print(vertex + " is connected to: ");
+            LinkedList<Edge> list = adjacencyList.get(vertex);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print("(" + list.get(i).getWeight() + ")" + list.get(i).getDestination() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void getShortestPath() {
+        int smallest = Integer.MAX_VALUE;
+        int index = Integer.MAX_VALUE;
+        for (List<String> LL : allPaths) {
+            int cost = 0;
+            for (int i = 1; i < LL.size(); i++) {
+                List<Edge> LL2 = adjacencyList.get(LL.get(i));
+                for (Edge e : LL2) {
+                    if (i < LL.size() - 1) {
+                        if ((LL.get(i + 1).toString().equalsIgnoreCase(e.destination.toString()))) {
+                            cost += e.weight;
+                        }
+                    }
+                }
+            }
+            if (cost < smallest) {
+                smallest = cost;
+                index = allPaths.indexOf(LL);
+            }
+        }
+        System.out.println("Actual Shortest Path with a distance of " + smallest + " meters : "
+                + String.join(" -> ", allPaths.get(index)));
+    }
+
+    public void printGraph2() {
+        System.out.println("Graph: (Adjacency Matrix)");
+        for (int i = 0; i < adjacencyList.size(); i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        System.out.println();
+        for (int i = 0; i < adjacencyList.size(); i++) {
+            System.out.println(i + " is connected to: ");
+            for (int j = 0; j < adjacencyList.size(); j++) {
+                if (matrix[i][j] > 0) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
